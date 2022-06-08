14a1724bb0e0aba1a0f268c88e5a251f74c8546a
demozx <734228006@qq.com>
2022-06-08 11:53:28 +0800

    仪表盘改动加系统信息

--- internal/controller/backend/welcome.go
+++ internal/controller/backend/welcome.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"gf_cms/api/backend"
 	"gf_cms/internal/service"
-	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/frame/g"
 	"runtime"
 )
@@ -16,19 +15,11 @@ var (
 type cWelcome struct{}
 
 func (c *cWelcome) Index(ctx context.Context, req *backend.WelcomeReq) (res *backend.WelcomeRes, err error) {
-	diskInfo := service.Runtime().GetDiskInfo()
-	diskInfo.All = gvar.New(gvar.New(diskInfo.All).Int() / 1024).String()
-	diskInfo.Used = gvar.New(gvar.New(diskInfo.Used).Int() / 1024).String()
-	memInfo := service.Runtime().GetMemInfo()
-	memInfo.Total = gvar.New(gvar.New(memInfo.Total).Int() / 1024 / 1024 / 1024).String()
-	memInfo.Used = gvar.New(gvar.New(memInfo.Used).Int() / 1024 / 1024 / 1024).String()
 	_ = g.RequestFromCtx(ctx).Response.WriteTpl("welcome/index.html", g.Map{
 		"project_name": service.ProjectName,
 		"system_root":  service.SystemRoot,
 		"host_info":    service.Runtime().GetHostInfo(),
 		"cpu_info":     service.Runtime().GetCpuInfo(),
-		"disk_info":    diskInfo,
-		"mem_info":     memInfo,
 		"go_version":   runtime.Version(),
 		"go_root":      runtime.GOROOT(),
 		"CPU_num":      runtime.NumCPU(),
