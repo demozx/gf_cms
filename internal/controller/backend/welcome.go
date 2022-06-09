db698862a7f20c9ce3bb4a254730e6120ff16b33
demozx <734228006@qq.com>
2022-06-09 09:48:26 +0800

    小改动

--- internal/controller/backend/welcome.go
+++ internal/controller/backend/welcome.go
@@ -16,15 +16,18 @@ type cWelcome struct{}
 
 func (c *cWelcome) Index(ctx context.Context, req *backend.WelcomeReq) (res *backend.WelcomeRes, err error) {
 	ip, _ := service.Util().GetLocalIP()
+	serverAddress, _ := g.Config().Get(ctx, "server.address")
+
 	_ = g.RequestFromCtx(ctx).Response.WriteTpl("backend/welcome/index.html", g.Map{
-		"project_name": service.ProjectName,
-		"system_root":  service.SystemRoot,
-		"host_info":    service.Runtime().GetHostInfo(),
-		"cpu_info":     service.Runtime().GetCpuInfo(),
-		"go_version":   runtime.Version(),
-		"go_root":      runtime.GOROOT(),
-		"CPU_num":      runtime.NumCPU(),
-		"ip":           ip,
+		"project_name":   service.ProjectName,
+		"system_root":    service.SystemRoot,
+		"host_info":      service.Runtime().GetHostInfo(),
+		"cpu_info":       service.Runtime().GetCpuInfo(),
+		"go_version":     runtime.Version(),
+		"go_root":        runtime.GOROOT(),
+		"CPU_num":        runtime.NumCPU(),
+		"ip":             ip,
+		"server_address": serverAddress,
 	})
 	return
 }
