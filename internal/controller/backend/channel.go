bcc4e0f51a19fca5d3de04a005a943de70e46666
demozx <734228006@qq.com>
2022-09-22 18:25:04 +0800

    栏目软删除

--- internal/controller/backend/channel.go
+++ internal/controller/backend/channel.go
@@ -26,7 +26,6 @@ func (c *cChannel) Index(ctx context.Context, req *backend.ChannelIndexReq) (res
 // Add 添加
 func (c *cChannel) Add(ctx context.Context, req *backend.ChannelAddReq) (res *backend.ChannelAddRes, err error) {
 	channelTree, err := service.Channel().BackendChannelTree(ctx)
-	g.Dump(channelTree)
 	err = service.Response().View(ctx, "backend/channel/add.html", g.Map{
 		"channelTree": channelTree,
 	})
