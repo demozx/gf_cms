61f61772530f8f936b8054f8d77580539d4a72b9
zhangxin <734228006@qq.com>
2022-11-08 14:49:52 +0800

    广告新增编辑优化

--- api/backendApi/adList.go
+++ api/backendApi/adList.go
@@ -21,7 +21,7 @@ type AdListAddReq struct {
 	g.Meta    `tags:"Backend" method:"post" summary:"后台广告添加"`
 	ChannelId int    `json:"channel_id" dc:"栏目id" arg:"true" v:"required|min:1#请选择所属分类|分类ID不能为0"`
 	Name      string `json:"name" dc:"广告名称" arg:"true" v:"required#请输入广告名称"`
-	Link      string `json:"link" dc:"链接地址" arg:"true" v:"required|url#请输入链接地址|链接地址格式错误"`
+	Link      string `json:"link" dc:"链接地址" arg:"true" v:""`
 	ImgUrl    string `json:"img_url" dc:"图片" v:""`
 	Status    int    `json:"status" dc:"状态(0停用,1显示)" v:"required|in:0,1#请选择是否启用|启用值不合法" d:"0"`
 	StartTime string `json:"start_time" dc:"广告开始时间" v:"required|datetime#请输入广告开始时间|广告开始时间格式错误"`
