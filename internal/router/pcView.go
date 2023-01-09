bdc5e383797030b15636b55405eeae71ae1e5de6
zx <734228006@qq.com>
2023-01-09 20:49:14 +0800

    pc路由美化

--- internal/router/pcView.go
+++ internal/router/pcView.go
@@ -28,20 +28,25 @@ func pcViewHandle(s *ghttp.Server) {
 			"/single_page/{id}.html" + pcHost:    pc.SinglePage.Detail, // 单页
 			"/search.html" + pcHost:              pc.Search.Index,      // 搜索
 			// 路由美化
-			"/about.html" + pcHost:              pc.RouterBeautify.About,              // 公司简介
-			"/news.html" + pcHost:               pc.RouterBeautify.News,               // 新闻动态
-			"/news_company.html" + pcHost:       pc.RouterBeautify.NewsCompany,        // 公司新闻
-			"/news_industry.html" + pcHost:      pc.RouterBeautify.NewsIndustry,       // 行业动态
-			"/product.html" + pcHost:            pc.RouterBeautify.Product,            // 产品展示
-			"/news/{id}.html" + pcHost:          pc.RouterBeautify.NewsDetail,         // 新闻详情
-			"/news_company/{id}.html" + pcHost:  pc.RouterBeautify.NewsCompanyDetail,  // 公司新闻详情
-			"/news_industry/{id}.html" + pcHost: pc.RouterBeautify.NewsIndustryDetail, // 行业动态详情
-			"/product_{id}.html" + pcHost:       pc.Image.List,                        // 子产品
-			"/product/{id}.html" + pcHost:       pc.RouterBeautify.ProductDetail,      // 产品详情
-			"/honor.html" + pcHost:              pc.RouterBeautify.Honor,              // 荣誉资质
-			"/honor/{id}.html" + pcHost:         pc.RouterBeautify.HonorDetail,        // 荣誉资质详情
-			"/guestbook.html" + pcHost:          pc.RouterBeautify.Guestbook,          // 在线留言
-			"/contact.html" + pcHost:            pc.RouterBeautify.Contact,            // 联系我们
+			"/about/" + pcHost:                   pc.RouterBeautify.About,              // 公司简介
+			"/about/index.html" + pcHost:         pc.RouterBeautify.About,              // 公司简介
+			"/news/" + pcHost:                    pc.RouterBeautify.News,               // 新闻动态
+			"/news/index.html" + pcHost:          pc.RouterBeautify.News,               // 新闻动态
+			"/news_company/" + pcHost:            pc.RouterBeautify.NewsCompany,        // 公司新闻
+			"/news_company/index.html" + pcHost:  pc.RouterBeautify.NewsCompany,        // 公司新闻
+			"/news_industry/" + pcHost:           pc.RouterBeautify.NewsIndustry,       // 行业动态
+			"/news_industry/index.html" + pcHost: pc.RouterBeautify.NewsIndustry,       // 行业动态
+			"/product/" + pcHost:                 pc.RouterBeautify.Product,            // 产品展示
+			"/product/index.html" + pcHost:       pc.RouterBeautify.Product,            // 产品展示
+			"/news/{id}.html" + pcHost:           pc.RouterBeautify.NewsDetail,         // 新闻详情
+			"/news_company/{id}.html" + pcHost:   pc.RouterBeautify.NewsCompanyDetail,  // 公司新闻详情
+			"/news_industry/{id}.html" + pcHost:  pc.RouterBeautify.NewsIndustryDetail, // 行业动态详情
+			"/product_{id}.html" + pcHost:        pc.Image.List,                        // 子产品
+			"/product/{id}.html" + pcHost:        pc.RouterBeautify.ProductDetail,      // 产品详情
+			"/honor.html" + pcHost:               pc.RouterBeautify.Honor,              // 荣誉资质
+			"/honor/{id}.html" + pcHost:          pc.RouterBeautify.HonorDetail,        // 荣誉资质详情
+			"/guestbook.html" + pcHost:           pc.RouterBeautify.Guestbook,          // 在线留言
+			"/contact.html" + pcHost:             pc.RouterBeautify.Contact,            // 联系我们
 		})
 	})
 }
