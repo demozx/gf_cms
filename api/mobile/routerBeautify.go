be2d5e5d93d0d6c34d711b11d0de73077a03c37f
zhangxin <734228006@qq.com>
2023-02-25 18:28:27 +0800

    移动端图集默认分页数

--- api/mobile/routerBeautify.go
+++ api/mobile/routerBeautify.go
@@ -1,7 +1,6 @@
 package mobile
 
 import (
-	"gf_cms/internal/model"
 	"github.com/gogf/gf/v2/frame/g"
 )
 
@@ -12,7 +11,8 @@ type RouterBeautifyDefaultRes struct{}
 
 type RouterBeautifyPageSizeReq struct {
 	g.Meta `tags:"Mobile" method:"get" summary:"mobile路由美化pageSizeReq"`
-	model.PageSizeReq
+	Page   int `json:"page" in:"query" d:"1"  v:"min:0#分页号码错误"     dc:"分页号码，默认1"`
+	Size   int `json:"size" in:"query" d:"10" v:"max:100#分页数量最大100条" dc:"分页数量，最大100"`
 }
 
 type RouterBeautifyDetailReq struct {
