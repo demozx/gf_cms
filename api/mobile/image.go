45c3238fa3ca24b04376534291fc68150ca9e649
zhangxin <734228006@qq.com>
2023-02-25 18:13:51 +0800

    移动端图集默认分页数

--- api/mobile/image.go
+++ api/mobile/image.go
@@ -9,7 +9,7 @@ type ImageListReq struct {
 	g.Meta    `tags:"Mobile" method:"get" summary:"mobile图集列表"`
 	ChannelId int `json:"id" dc:"图集栏目id" d:"1"`
 	Page      int `json:"page" in:"query" d:"1"  v:"min:0#分页号码错误"     dc:"分页号码，默认1"`
-	Size      int `json:"size" in:"query" d:"15" v:"max:100#分页数量最大100条" dc:"分页数量，最大100"`
+	Size      int `json:"size" in:"query" d:"10" v:"max:100#分页数量最大100条" dc:"分页数量，最大100"`
 }
 type ImageListRes struct {
 	List  []*model.ImageListItem
